0ba818ff7ebb4d7db25442e510f98b8b17e91180
zyf993 <zyf19970419@gmail.com>
2021-11-01 21:41:41 -0400

    Update Unittest.py

--- Project 3/Unittest.py
+++ Project 3/Unittest.py
@@ -1,16 +1,20 @@
 from google.cloud import language
 from Google_analysis import analyze_text_sentiment
-import unittest
+from tweet_search_api import twitter_content
+import unittest 
 
-
-class TestCaseRun(unittest.TestCase):
+class TestStringMethods(unittest.TestCase):
 	def test_sentiment_score(self):
 	    S = analyze_text_sentiment(text)
-	    # print(S)
-	    if S <= 0:
-	        print("Failed, the score is not positive")
-	    else:
-	        print("Passed, the score is positive")
+	    # assert S >= 0
+	    msg = 'score is smaller than zero'
+	    self.assertTrue(S >= 0, msg)
+
+	def test_negative(self):
+	    # error message in case if test case got failed 
+	    message = "Test value is none"
+	    # assertIsNotNone() to check that if input value is not none 
+	    self.assertIsNotNone(twitter_content, message) 
 
 if __name__ == '__main__':
     f = open("tweets.txt","r",encoding='utf-8')
@@ -18,4 +22,5 @@ if __name__ == '__main__':
     f.close()
     #can be import from tweet file
     analyze_text_sentiment(text)
+    #call the unit test function
     unittest.main()
